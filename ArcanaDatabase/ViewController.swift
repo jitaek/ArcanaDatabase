e0e52d1159459d31dc4cd374bb052ef99ef06fd3
jitae <jitaekim93@gmail.com>
2016-09-17 15:38:30 -0700

    Initial Commit

---
+++ ArcanaDatabase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArcanaDatabase
+//
+//  Created by Jitae Kim on 9/17/16.
+//  Copyright © 2016 Jitae Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
